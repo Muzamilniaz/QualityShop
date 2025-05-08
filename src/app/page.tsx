3f1b0841646db6bbd6536fa4e0aba5988da53fb9
muzamil-atrule <muzamilniaz.mn@gmail.com>
2025-05-08 14:02:17 +0500

    build created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import BannerSlider from "@/components/homepage/BannerSlider";
-import ProductsGrid from "@/components/homepage/ProductsGrid";
 import PopularCategories from "@/components/homepage/PopularCategories";
 import PromoSection from "@/components/homepage/PromoSection";
 import FoodCategories from "@/components/homepage/FoodCategories";
